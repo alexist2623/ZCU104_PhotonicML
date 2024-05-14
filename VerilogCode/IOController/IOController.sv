fe321efa5d2e737d332f43d973927d8f4ebee5d0
ParkJeonghyun <49219392+alexist2623@users.noreply.github.com>
2024-05-14 18:56:36 +0900

    IO Controller codes are written

---
+++ VerilogCode/IOController/IOController.sv
@@ -0,0 +1,170 @@
+module IOController
+#(
+    //////////////////////////////////////////////////////////////////////////////////
+    // SignalShifter Interface
+    //////////////////////////////////////////////////////////////////////////////////
+    parameter MAX_DELAY                     = 1000000000,
+    parameter DELAY_WIDTH                   = $clog2(MAX_DELAY),
+    parameter MAX_EVENT                     = 10,
+    parameter EVENT_WIDTH                   = $clog2(MAX_EVENT),
+    //////////////////////////////////////////////////////////////////////////////////
+    // AXI4 Configuraiton
+    //////////////////////////////////////////////////////////////////////////////////
+    parameter AXI_ADDR_WIDTH                = 6,
+    parameter AXI_DATA_WIDTH                = 128,
+    parameter AXI_STROBE_WIDTH              = AXI_DATA_WIDTH >> 3,
+    parameter AXI_STROBE_LEN                = 4 // LOG(AXI_STROBE_WDITH)
+)
+(
+    //////////////////////////////////////////////////////////////////////////////////
+    // AXI4 Address Write
+    //////////////////////////////////////////////////////////////////////////////////
+    input  wire [AXI_ADDR_WIDTH - 1:0] s_axi_awaddr,
+    input  wire [15:0] s_axi_awid, 
+    input  wire [1:0] s_axi_awburst,
+    input  wire [2:0] s_axi_awsize,
+    input  wire [7:0] s_axi_awlen,
+    input  wire s_axi_awvalid,
+    input  wire [15:0] s_axi_awuser, // added to resolve wrapping error
+    output wire s_axi_awready,                                                        //Note that ready signal is wire
+    
+    //////////////////////////////////////////////////////////////////////////////////
+    // AXI4 Write Response
+    //////////////////////////////////////////////////////////////////////////////////
+    input  wire s_axi_bready,
+    output wire [1:0] s_axi_bresp,
+    output wire s_axi_bvalid,
+    output wire [15:0] s_axi_bid, // added to resolve wrapping error
+    
+    //////////////////////////////////////////////////////////////////////////////////
+    // AXI4 Data Write
+    //////////////////////////////////////////////////////////////////////////////////
+    input  wire [AXI_DATA_WIDTH - 1:0] s_axi_wdata,
+    input  wire [AXI_STROBE_WIDTH - 1:0] s_axi_wstrb,
+    input  wire s_axi_wvalid,
+    input  wire s_axi_wlast,
+    output wire s_axi_wready,                                                        //Note that ready signal is wire
+    
+    //////////////////////////////////////////////////////////////////////////////////
+    // AXI4 Address Read
+    //////////////////////////////////////////////////////////////////////////////////
+    input  wire [1:0] s_axi_arburst,
+    input  wire [7:0] s_axi_arlen,
+    input  wire [AXI_ADDR_WIDTH - 1:0] s_axi_araddr,
+    input  wire [2:0] s_axi_arsize,
+    input  wire s_axi_arvalid,
+    input  wire [15:0] s_axi_arid, // added to resolve wrapping error
+    input  wire [15:0] s_axi_aruser, // added to resolve wrapping error
+    output wire s_axi_arready,
+    output wire [15:0] s_axi_rid, // added to resolve wrapping error
+    
+    //////////////////////////////////////////////////////////////////////////////////
+    // AXI4 Data Read
+    //////////////////////////////////////////////////////////////////////////////////
+    input  wire s_axi_rready,
+    output wire [AXI_DATA_WIDTH - 1:0] s_axi_rdata,
+    output wire [1:0] s_axi_rresp,
+    output wire s_axi_rvalid,
+    output wire s_axi_rlast,
+    
+    //////////////////////////////////////////////////////////////////////////////////
+    // AXI4 Clock
+    //////////////////////////////////////////////////////////////////////////////////
+    input  wire s_axi_aclk,
+    
+    //////////////////////////////////////////////////////////////////////////////////
+    // AXI4 Reset
+    //////////////////////////////////////////////////////////////////////////////////
+    input  wire s_axi_aresetn,
+    
+    //////////////////////////////////////////////////////////////////////////////////
+    // SignalShifter Reset
+    //////////////////////////////////////////////////////////////////////////////////
+    input  wire auto_start,
+    input  wire input_signal,
+    output wire output_signal
+);
+
+wire reset;
+wire [DELAY_WIDTH-1:0] delay_value;
+wire delay_set;
+wire [EVENT_WIDTH-1:0] event_value;
+wire event_set;
+wire event_polarity_set;
+
+AXI2IOControllerCOM #(
+    .AXI_ADDR_WIDTH                         (AXI_ADDR_WIDTH),
+    .AXI_DATA_WIDTH                         (AXI_DATA_WIDTH),
+    .AXI_STROBE_WIDTH                       (AXI_STROBE_WIDTH),
+    .AXI_STROBE_LEN                         (AXI_STROBE_LEN),
+    .MAX_DELAY                              (MAX_DELAY),
+    .DELAY_WIDTH                            (DELAY_WIDTH),
+    .MAX_EVENT                              (MAX_EVENT),
+    .EVENT_WIDTH                            (EVENT_WIDTH)
+) axi2iocontrollercom_0 (
+    .s_axi_awaddr                           (s_axi_awaddr),
+    .s_axi_awid                             (s_axi_awid),
+    .s_axi_awburst                          (s_axi_awburst),
+    .s_axi_awsize                           (s_axi_awsize),
+    .s_axi_awlen                            (s_axi_awlen),
+    .s_axi_awvalid                          (s_axi_awvalid),
+    .s_axi_awuser                           (s_axi_awuser),
+    .s_axi_awready                          (s_axi_awready),
+    
+    .s_axi_bready                           (s_axi_bready),
+    .s_axi_bresp                            (s_axi_bresp),
+    .s_axi_bvalid                           (s_axi_bvalid),
+    .s_axi_bid                              (s_axi_bid),
+    
+    .s_axi_wdata                            (s_axi_wdata),
+    .s_axi_wstrb                            (s_axi_wstrb),
+    .s_axi_wvalid                           (s_axi_wvalid),
+    .s_axi_wlast                            (s_axi_wlast),
+    .s_axi_wready                           (s_axi_wready),
+    
+    .s_axi_arburst                          (s_axi_arburst),
+    .s_axi_arlen                            (s_axi_arlen),
+    .s_axi_araddr                           (s_axi_araddr),
+    .s_axi_arsize                           (s_axi_arsize),
+    .s_axi_arvalid                          (s_axi_arvalid),
+    .s_axi_arid                             (s_axi_arid),
+    .s_axi_aruser                           (s_axi_aruser),
+    .s_axi_arready                          (s_axi_arready),
+    .s_axi_rid                              (s_axi_rid),
+    
+    .s_axi_rready                           (s_axi_rready),
+    .s_axi_rdata                            (s_axi_rdata),
+    .s_axi_rresp                            (s_axi_rresp),
+    .s_axi_rvalid                           (s_axi_rvalid),
+    .s_axi_rlast                            (s_axi_rlast),
+    
+    .s_axi_aclk                             (s_axi_aclk),
+    .s_axi_aresetn                          (s_axi_aresetn),
+    
+    .reset                                  (reset),
+    .delay_value                            (delay_value),
+    .delay_set                              (delay_set),
+    .event_value                            (event_value),
+    .event_set                              (event_set),
+    .event_polarity_set                     (event_polarity_set)
+);
+
+SignalShifter #(
+    .MAX_DELAY                              (MAX_DELAY),
+    .DELAY_WIDTH                            (DELAY_WIDTH),
+    .MAX_EVENT                              (MAX_EVENT),
+    .EVENT_WIDTH                            (EVENT_WIDTH)
+) signal_shifter_0 (
+    .auto_start                             (auto_start),
+    .clk                                    (s_axi_aclk),
+    .reset                                  (reset),
+    .delay_value                            (delay_value),
+    .delay_set                              (delay_set),
+    .event_value                            (event_value),
+    .event_set                              (event_set),
+    .event_polarity_set                     (event_polarity_set),
+    .input_signal                           (input_signal),
+    .output_signal                          (output_signal)
+);
+
+endmodule
---
+++ VerilogCode/IOController/SignalShifter.sv
@@ -0,0 +1,97 @@
+module SignalShifter
+#(
+    parameter MAX_DELAY                     = 1000000000,
+    parameter DELAY_WIDTH                   = $clog2(MAX_DELAY),
+    parameter MAX_EVENT                     = 10,
+    parameter EVENT_WIDTH                   = $clog2(MAX_EVENT)
+)
+(
+    input  wire auto_start,
+    input  wire clk,
+    input  wire reset,
+    input  wire [DELAY_WIDTH-1:0] delay_value,
+    input  wire delay_set,
+    input  wire [EVENT_WIDTH-1:0] event_value,
+    input  wire event_set,
+    input  wire event_polarity_set,
+    input  wire input_signal,
+    output reg  output_signal
+);
+
+localparam STATE_NUM                        = 4;
+localparam STATE_WIDTH                      = $clog2(STATE_NUM);
+localparam IDLE                             = STATE_WIDTH'(0);
+localparam DELAY_COUNT                      = STATE_WIDTH'(1);
+
+reg  input_signal_buffer1;
+reg  input_signal_buffer2;
+reg  input_signal_buffer3;
+reg  [DELAY_WIDTH-1:0] delay_counter;
+reg  [DELAY_WIDTH-1:0] delay_counter_reg;
+reg  [EVENT_WIDTH-1:0] event_counter;
+reg  [EVENT_WIDTH-1:0] event_counter_reg;
+reg  event_polarity;
+reg  [STATE_WIDTH-1:0] signal_shifter_state;
+
+always_ff @(posedge clk) begin
+    if( reset == 1'b1 )begin
+        input_signal_buffer1 <= 1'b0;
+        input_signal_buffer2 <= 1'b0;
+        delay_counter <= DELAY_WIDTH'(0);
+        delay_counter_reg <= DELAY_WIDTH'(0);
+        event_counter <= EVENT_WIDTH'(0);
+        event_counter_reg <= EVENT_WIDTH'(0);
+        output_signal <= 1'b0;
+        event_polarity <= 1'b0;
+        signal_shifter_state <= IDLE;
+    end
+    else begin
+        output_signal <= 1'b0;
+        /*
+         * buffer3 is for edge sense
+         * buffer1, 2 is to synchronization to clk region
+         */
+        {input_signal_buffer3, input_signal_buffer2, input_signal_buffer1} <= {input_signal_buffer2, input_signal_buffer1, input_signal};
+        if( delay_set == 1'b1 ) begin
+            delay_counter_reg <= delay_value;
+        end
+        
+        if( event_set == 1'b1 ) begin
+            event_counter_reg <= event_value;
+        end
+        
+        if( auto_start ) begin
+            case(signal_shifter_state)
+                IDLE: begin
+                    /*
+                     * event_polarity = 1 : Sense HIGH -> LOW
+                     * event_polarity = 0 : Sense LOW  -> HIGH
+                     */
+                    if( (event_polarity ^ input_signal_buffer2 == 0) && (event_polarity ^ input_signal_buffer3 == 1) ) begin
+                        event_counter <= event_counter + 1;
+                        $display("EDGE DETECTED");
+                    end
+                    
+                    if( event_counter >= event_counter_reg ) begin
+                        delay_counter <= 1;
+                        event_counter <= 0;
+                        signal_shifter_state <= DELAY_COUNT;
+                        $display("SIGNAL SHIFTER ENTERS TO DELAY COUNT");
+                    end
+                end
+                DELAY_COUNT: begin
+                    delay_counter <= delay_counter + 1;
+                    if( delay_counter >= delay_counter_reg ) begin
+                        delay_counter <= 0;
+                        event_counter <= 0;
+                        output_signal <= 1'b1;
+                        signal_shifter_state <= IDLE;
+                        $display("SIGNAL SHIFTER OUTPUT SIGNAL");
+                    end
+                end
+            endcase
+        end
+    end
+end
+
+endmodule
