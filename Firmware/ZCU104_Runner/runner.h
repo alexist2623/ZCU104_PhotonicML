c3d0fdc8828486290d6a22c149b4061d802c31ea
ParkJeonghyun <49219392+alexist2623@users.noreply.github.com>
2024-05-26 15:10:52 +0900

    IO Controller added

--- Firmware/ZCU104_Runner/runner.h
+++ Firmware/ZCU104_Runner/runner.h
@@ -14,10 +14,10 @@
 #include "ff.h"          // FatFs library
 
 /****************** Define Interrupt ID for each function ********************/
-#define INT_ID_STOP_DISPLAY 		0x0
-#define INT_ID_RUN_DISPLAY 			0x1
-#define INT_ID_LOAD_SD_CARD			0x2
-#define INT_ID_SET_NEW_IMAGE		0x3
+#define INT_ID_STOP_DISPLAY 			0x0
+#define INT_ID_RUN_DISPLAY 				0x1
+#define INT_ID_LOAD_SD_CARD				0x2
+#define INT_ID_SET_NEW_IMAGE			0x3
 
 
 /************************** Constant Definitions *****************************/
@@ -28,33 +28,41 @@
  * change all the needed parameters in one place.
  */
 //0xF9020000U
-#define CPU_BASEADDR				XPAR_SCUGIC_0_CPU_BASEADDR
+#define CPU_BASEADDR					XPAR_SCUGIC_0_CPU_BASEADDR
 //0xF9010000U
-#define DIST_BASEADDR				XPAR_SCUGIC_0_DIST_BASEADDR
-#define GIC_DEVICE_INT_MASK        	0x00010000
-#define PL_INTID					121
+#define DIST_BASEADDR					XPAR_SCUGIC_0_DIST_BASEADDR
+#define GIC_DEVICE_INT_MASK        		0x00010000
+#define PL_INTID						121
 
 // Note that XPAR_ constants are defined in xparameters.h files which is dependent on xsa file
-#define MASTER_CONTROLLER_ADDR		XPAR_MASTERCONTROLLER_0_BASEADDR
-#define IMAGE_CONTROLLER_ADDR		XPAR_IMAGECONTROLLER_0_BASEADDR
-#define SET_IMAGE_SIZE              (XPAR_IMAGECONTROLLER_0_BASEADDR | 0x20U)
-#define IMAGE_DATA_DONE_ADDR		(XPAR_IMAGECONTROLLER_0_BASEADDR | 0x30U)
-#define IMAGE_DATA_WRITE_ADDR		(XPAR_IMAGECONTROLLER_0_BASEADDR | 0x40U)
-#define SET_NEW_IMAGE_ADDR			(XPAR_IMAGECONTROLLER_0_BASEADDR | 0x50U)
-#define DEASSERT_IRQ_ADDR			(XPAR_IMAGECONTROLLER_0_BASEADDR | 0x60U)
+#define MASTER_CONTROLLER_ADDR			XPAR_MASTERCONTROLLER_0_BASEADDR
+#define CAMERAEXPOSUREEND_ADDR 			XPAR_CAMERAEXPOSUREEND_BASEADDR
+#define CAMERAEXPOSUREEND_DELAY			(XPAR_CAMERAEXPOSUREEND_BASEADDR | 0x10U)
+#define CAMERAEXPOSUREEND_EVENT			(XPAR_CAMERAEXPOSUREEND_BASEADDR | 0x20U)
+#define CAMERAEXPOSUREEND_POLARITY		(XPAR_CAMERAEXPOSUREEND_BASEADDR | 0x30U)
+#define CAMERAEXPOSURESTART_ADDR 		XPAR_CAMERAEXPOSURESTART_BASEADDR
+#define CAMERAEXPOSURESTART_DELAY		(XPAR_CAMERAEXPOSURESTART_BASEADDR | 0x10U)
+#define CAMERAEXPOSURESTART_EVENT 		(XPAR_CAMERAEXPOSURESTART_BASEADDR | 0x20U)
+#define CAMERAEXPOSURESTART_POLARITY 	(XPAR_CAMERAEXPOSURESTART_BASEADDR | 0x30U)
+#define IMAGE_CONTROLLER_ADDR			XPAR_IMAGECONTROLLER_0_BASEADDR
+#define SET_IMAGE_SIZE              	(XPAR_IMAGECONTROLLER_0_BASEADDR | 0x20U)
+#define IMAGE_DATA_DONE_ADDR			(XPAR_IMAGECONTROLLER_0_BASEADDR | 0x30U)
+#define IMAGE_DATA_WRITE_ADDR			(XPAR_IMAGECONTROLLER_0_BASEADDR | 0x40U)
+#define SET_NEW_IMAGE_ADDR				(XPAR_IMAGECONTROLLER_0_BASEADDR | 0x50U)
+#define DEASSERT_IRQ_ADDR				(XPAR_IMAGECONTROLLER_0_BASEADDR | 0x60U)
 
-#define DATA_SAVE_MEM_ADDR			0x2000000U
-#define IMAGE_WRITE_TIME			625
-#define S_AXI_WDATA_SIZE			16
-#define SCREEN_WIDTH				100
-#define SCREEN_HEIGHT				100
-#define IMAGE_SIZE					10000 // 100 x 100 size image
+#define DATA_SAVE_MEM_ADDR				0x2000000U
+#define IMAGE_WRITE_TIME				625
+#define S_AXI_WDATA_SIZE				16
+#define SCREEN_WIDTH					100
+#define SCREEN_HEIGHT					100
+#define IMAGE_SIZE						10000 // 100 x 100 size image
 
 /************************** 128 bit make macro *******************************/
-#define MAKE128CONST(hi,lo) ((((__uint128_t)hi << 64) | (lo)))
-#define UPPER(x) (uint64_t)( ( (x) >> 64 ) & MASK64BIT)
-#define LOWER(x) (uint64_t)( (x) & MASK64BIT )
-#define MASK64BIT ((uint64_t) 0xffffffffffffffff)
+#define MAKE128CONST(hi,lo) 			((((__uint128_t)hi << 64) | (lo)))
+#define UPPER(x) 						(uint64_t)( ( (x) >> 64 ) & MASK64BIT)
+#define LOWER(x) 						(uint64_t)( (x) & MASK64BIT )
+#define MASK64BIT 						((uint64_t) 0xffffffffffffffff)
 
 static INLINE void Xil_Out128(UINTPTR Addr, __uint128_t Value)
 {
@@ -87,6 +95,14 @@ void auto_start();
 void auto_stop();
 void reset_enable();
 void reset_disable();
+
+/***************** IOControllerDriver Function Prototypes ********************/
+void set_camera_exposure_start_delay(uint64_t delay_value);
+void set_camera_exposure_start_event(uint64_t event_value);
+void set_camera_exposure_start_polarity(uint64_t polarity_value);
+void set_camera_exposure_end_delay(uint64_t delay_value);
+void set_camera_exposure_end_event(uint64_t event_value);
+void set_camera_exposure_end_polarity(uint64_t polarity_value);
 
 /********************* sdcard.c Function Prototypes **************************/
 int init_sd_card(XSdPs *SdInstance, FATFS *FS_Instance);
