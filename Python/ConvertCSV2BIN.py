b02b5d868d0e2fb7419695c26ff0d5b637786a43
ParkJeonghyun <49219392+alexist2623@users.noreply.github.com>
2024-05-27 13:27:29 +0900

    Blank teset image added

--- Python/ConvertCSV2BIN.py
+++ Python/ConvertCSV2BIN.py
@@ -8,7 +8,30 @@ Created on Wed May  8 18:02:06 2024
 import csv
 import struct
 from itertools import islice
-
+def make01BinaryDataFromCSV(row_num, csv_file_path, output_folder):
+    with open(csv_file_path, 'r', newline='') as file:
+        reader = csv.reader(file)
+        if row_num % 2 == 0:
+            row = [0 for j in range(10001)]
+            
+        else:
+            row = [255 for j in range(10001)]
+            
+        row_number = row_num
+        binary_data = row[1:]
+        # j = 0
+        # for b in binary_data:
+        #     if int(b) == 255:
+        #         print(str(1)+' ', end = '')
+        #     else:
+        #         print(str(0)+' ', end = '')
+        #     j += 1
+        #     if( j%100 == 0 ):
+        #         print()
+        byte_array = bytearray(int(b) for b in binary_data)
+        with open(f'{output_folder}/test{str(row_num).zfill(4)}.bin', 'wb') as bin_file:
+            bin_file.write(byte_array)
+            
 def saveBinaryDataFromCSV(row_num, csv_file_path, output_folder):
     with open(csv_file_path, 'r', newline='') as file:
         reader = csv.reader(file)
@@ -30,6 +53,6 @@ def saveBinaryDataFromCSV(row_num, csv_file_path, output_folder):
 
 if __name__ == "__main__":
     csv_file_path = 'MNIST_subset_1000_train.csv'
-    output_folder = f'TEST_DATA'
+    output_folder = f'TEST_DATA01'
     for row_num in range(1000):
-        saveBinaryDataFromCSV(row_num, csv_file_path, output_folder)
+        make01BinaryDataFromCSV(row_num, csv_file_path, output_folder)
