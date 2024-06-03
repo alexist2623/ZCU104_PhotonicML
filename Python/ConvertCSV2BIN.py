1c7cce72976b90b7229d775016b6297574935ff7
ParkJeonghyun <49219392+alexist2623@users.noreply.github.com>
2024-06-03 17:07:55 +0900

    function for linear data creation added

--- Python/ConvertCSV2BIN.py
+++ Python/ConvertCSV2BIN.py
@@ -8,6 +8,26 @@ Created on Wed May  8 18:02:06 2024
 import csv
 import struct
 from itertools import islice
+def makeLinearDataFromCSV(row_num, csv_file_path, output_folder):
+    with open(csv_file_path, 'r', newline='') as file:
+        reader = csv.reader(file)
+        row = [int(row_num) % 256 for j in range(10001)]
+            
+        row_number = row_num
+        binary_data = row[1:]
+        # j = 0
+        # for b in binary_data:
+        #     if int(b) == 255:
+        #         print(str(1)+' ', end = '')
+        #     else:
+        #         print(str(0)+' ', end = '')
+        #     j += 1
+        #     if( j%100 == 0 ):
+        #         print()
+        byte_array = bytearray(int(b) for b in binary_data)
+        with open(f'{output_folder}/test{str(row_num).zfill(4)}.bin', 'wb') as bin_file:
+            bin_file.write(byte_array)
+
 def make01BinaryDataFromCSV(row_num, csv_file_path, output_folder):
     with open(csv_file_path, 'r', newline='') as file:
         reader = csv.reader(file)
@@ -15,7 +35,7 @@ def make01BinaryDataFromCSV(row_num, csv_file_path, output_folder):
             row = [0 for j in range(10001)]
             
         else:
-            row = [255 for j in range(10001)]
+            row = [10 for j in range(10001)]
             
         row_number = row_num
         binary_data = row[1:]
@@ -38,6 +58,12 @@ def saveBinaryDataFromCSV(row_num, csv_file_path, output_folder):
         row = next(islice(reader, row_num, row_num + 1), None)
         row_number = row[0]
         binary_data = row[1:]
+        for i in range(len(binary_data)):
+            if int(binary_data[i]) == 0:
+                binary_data[i] = 50
+            else:
+                binary_data[i] = 200
+            
         # j = 0
         # for b in binary_data:
         #     if int(b) == 255:
@@ -53,6 +79,6 @@ def saveBinaryDataFromCSV(row_num, csv_file_path, output_folder):
 
 if __name__ == "__main__":
     csv_file_path = 'MNIST_subset_1000_train.csv'
-    output_folder = f'TEST_DATA01'
+    output_folder = f'TEST_DATA0_10'
     for row_num in range(1000):
         make01BinaryDataFromCSV(row_num, csv_file_path, output_folder)
